5a88b99ad82395a94b65d65808933cd82428e5fa
yashvardhan singh tomar <yashvardhantomar2000@gmail.com>
2022-03-12 00:55:00 +0530

    updated

---
+++ app/src/main/java/com/example/instagramclone/fragments/AddFragment.kt
@@ -0,0 +1,13 @@
+package com.example.instagramclone.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.instagramclone.R
+
+
+class AddFragment : Fragment(R.layout.fragment_add) {
+
+}
